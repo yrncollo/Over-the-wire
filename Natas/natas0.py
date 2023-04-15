9b03691569550be2d5ffac512acf6fc15c7a8fbc
yrncollo <ngenocollins599@gmail.com>
2023-04-15 16:42:45 +0300

    prints natas0 password

---
+++ Natas/natas0.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+password = "natas0"
+username = "natas0"
+url = f"http://{username}.natas.labs.overthewire.org"
+
+""" Pulling data from the website"""
+data = requests.post(url, auth=(username, password))
+soup = BeautifulSoup(data.content, "html.parser")
+
+print(soup)
+result = re.findall(r'[A-Za-z0-9]{32}', data.text)[0]
+print(result)
